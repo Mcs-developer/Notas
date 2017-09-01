dfc12ce601200041b4f503a5628b52bd4d0e173f
Marlon Chara Salazar <marlsas0306@gmail.com>
2017-09-01 09:58:14 -0500

    se agregaron pesatañas a la vista principañ

--- app/src/main/java/com/ucc/notas/MainActivity.java
+++ app/src/main/java/com/ucc/notas/MainActivity.java
@@ -2,12 +2,37 @@ package com.ucc.notas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
-
+    TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TabHost host = (TabHost) findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("C1");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("C2");
+        host.addTab(spec);
+
+        //Tab 3
+        spec = host.newTabSpec("Tab Three");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("C3");
+        host.addTab(spec);
     }
 }
